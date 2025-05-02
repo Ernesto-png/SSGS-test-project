471226b4b7415d6bdcc9f69e8d5afd7aa22e9b01
Ernesto-png <80959709+Ernesto-png@users.noreply.github.com>
2025-05-02 12:25:56 +0200

    Create jest.config.js
    added coverage code

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  collectCoverage: true, // Attiva la raccolta del coverage
+  coverageDirectory: 'coverage', // Directory per i report
+  coverageReporters: ['text', 'lcov'], // Report in formato testo e HTML
+};
